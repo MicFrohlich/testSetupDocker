ed0c567d450169731b19a67e39a51e99ff0ee4fa
Spazzy757 <brendankamp757@gmail.com>
2017-11-14 10:12:34 +0200

    Added a admin interface to Posts model

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from posts.models import Post
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Post)
